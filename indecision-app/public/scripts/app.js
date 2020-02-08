184d340042a65b6ff5683fa89b023a1326f92a33
Ketan <ketantoo@gmail.com>
2020-02-08 12:54:22 +0530

    app creted

--- indecision-app/public/scripts/app.js
+++
@@ -1,8 +0,0 @@
-console.log("App.js is running");
-
-// var template = <p>THis is JSX from app.js</p>;
-
-ReactDOM.render(
-  React.createElement("h1", { id: "title" }, "Hi this is compiled jsx"),
-  document.getElementById("root")
-);
